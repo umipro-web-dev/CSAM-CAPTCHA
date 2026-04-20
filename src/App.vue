555d29a68f49c94b41d04fe4c565530242c01bae
umipro-web-dev <113771739+umipro-web-dev@users.noreply.github.com>
2026-04-20 16:24:52 +0900

    Remove CaptchaWidget from App.vue
    Removed CaptchaWidget component from App.vue.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,7 @@
 <template>
   <div class="app-container">
-    <CaptchaWidget />
   </div>
 </template>
-
-<script setup>
-import CaptchaWidget from './components/CaptchaWidget.vue'
-</script>
 
 <style scoped>
 .app-container {
